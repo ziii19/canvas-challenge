ad76e11d3b4beccb909bf396889f24fb2e3d09bb
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-29 09:17:07 +0700

    feat: add animation in draw line

--- lib/canvas_challenge.dart
+++ lib/canvas_challenge.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class CanvasChallenge extends StatefulWidget {
@@ -7,7 +9,8 @@ class CanvasChallenge extends StatefulWidget {
   State<CanvasChallenge> createState() => _CanvasChallengeState();
 }
 
-class _CanvasChallengeState extends State<CanvasChallenge> {
+class _CanvasChallengeState extends State<CanvasChallenge>
+    with SingleTickerProviderStateMixin {
   List<Color> containerColors = [
     Colors.blue,
     Colors.red,
@@ -22,6 +25,27 @@ class _CanvasChallengeState extends State<CanvasChallenge> {
 
   final List<GlobalKey> leftKeys = List.generate(4, (_) => GlobalKey());
   final List<GlobalKey> rightKeys = List.generate(4, (_) => GlobalKey());
+
+  late AnimationController _animationController;
+  int? currentAnimatingIndex; // Track which connection is being animated
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+    _animationController.addListener(() {
+      setState(() {}); // Update UI whenever animation progresses
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,10 +64,21 @@ class _CanvasChallengeState extends State<CanvasChallenge> {
 
             return Stack(
               children: [
-                CustomPaint(
-                  painter: CurvePainter(
-                      connections, containerColors, leftKeys, rightKeys),
-                  size: Size(constraints.maxWidth, constraints.maxHeight),
+                AnimatedBuilder(
+                  animation: _animationController,
+                  builder: (context, child) {
+                    return CustomPaint(
+                      painter: CurvePainter(
+                        connections,
+                        containerColors,
+                        leftKeys,
+                        rightKeys,
+                        _animationController.value,
+                        currentAnimatingIndex, // Pass the current animation index
+                      ),
+                      size: Size(constraints.maxWidth, constraints.maxHeight),
+                    );
+                  },
                 ),
                 Padding(
                   padding: const EdgeInsets.all(20.0),
@@ -77,7 +112,7 @@ class _CanvasChallengeState extends State<CanvasChallenge> {
                                             leftSelectedIndex == index)
                                         ? containerColors[index]
                                         : Colors.transparent,
-                                    width: 8,
+                                    width: 10,
                                   ),
                                   borderRadius: BorderRadius.circular(8),
                                 ),
@@ -98,9 +133,16 @@ class _CanvasChallengeState extends State<CanvasChallenge> {
                                       (element) => element == index);
 
                                   if (leftIndex != -1) {
+                                    // Disconnect if already connected
                                     connections[leftIndex] = null;
                                   } else if (leftSelectedIndex != null) {
+                                    // Make the connection and start the animation
                                     connections[leftSelectedIndex!] = index;
+                                    currentAnimatingIndex = leftSelectedIndex;
+
+                                    // Reset and start the animation
+                                    _animationController.reset();
+                                    _animationController.forward();
                                   }
                                 });
                               },
@@ -114,7 +156,7 @@ class _CanvasChallengeState extends State<CanvasChallenge> {
                                       ? Border.all(
                                           color: containerColors[
                                               connections.indexOf(index)],
-                                          width: 8,
+                                          width: 10,
                                         )
                                       : null,
                                   borderRadius: BorderRadius.circular(8),
@@ -141,9 +183,11 @@ class CurvePainter extends CustomPainter {
   final List<Color> containerColors;
   final List<GlobalKey> leftKeys;
   final List<GlobalKey> rightKeys;
+  final double progress;
+  final int? animatingIndex;
 
-  CurvePainter(
-      this.connections, this.containerColors, this.leftKeys, this.rightKeys);
+  CurvePainter(this.connections, this.containerColors, this.leftKeys,
+      this.rightKeys, this.progress, this.animatingIndex);
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -153,7 +197,7 @@ class CurvePainter extends CustomPainter {
 
         final paint = Paint()
           ..color = containerColors[i]
-          ..strokeWidth = 8
+          ..strokeWidth = 10
           ..style = PaintingStyle.stroke;
 
         final p1 = _getContainerPosition(leftKeys[i]);
@@ -167,7 +211,16 @@ class CurvePainter extends CustomPainter {
           ..moveTo(p1.dx, p1.dy)
           ..cubicTo(cp1.dx + 50, p1.dy, cp2.dx, cp2.dy, p2.dx - 110, p2.dy);
 
-        canvas.drawPath(path, paint);
+        if (i == animatingIndex) {
+          PathMetrics pathMetrics = path.computeMetrics();
+          for (PathMetric pathMetric in pathMetrics) {
+            Path extractPath =
+                pathMetric.extractPath(0.0, pathMetric.length * progress);
+            canvas.drawPath(extractPath, paint);
+          }
+        } else {
+          canvas.drawPath(path, paint);
+        }
       }
     }
   }
@@ -185,6 +238,8 @@ class CurvePainter extends CustomPainter {
 
   @override
   bool shouldRepaint(CurvePainter oldDelegate) {
-    return oldDelegate.connections != connections;
+    return oldDelegate.connections != connections ||
+        oldDelegate.progress != progress ||
+        oldDelegate.animatingIndex != animatingIndex;
   }
 }
